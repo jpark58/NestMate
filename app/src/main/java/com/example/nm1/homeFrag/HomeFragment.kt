f1b8273922d726a2713d8480c00324194270f719
2018008222 <ekfvnddl99@hanyang.ac.kr>
2021-03-03 15:47:26 +0900

    init

--- app/src/main/java/com/example/nm1/homeFrag/HomeFragment.kt
+++ app/src/main/java/com/example/nm1/homeFrag/HomeFragment.kt
@@ -7,7 +7,9 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.annotation.RequiresApi
 import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.nm1.R
+import kotlinx.android.synthetic.main.home_fragment.*
 
 class HomeFragment : Fragment() {
     override fun onCreate(savedInstanceState: Bundle?) {
